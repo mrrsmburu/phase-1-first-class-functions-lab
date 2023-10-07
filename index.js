38e997ef8c7231db4ed6c7d11356d409e419ea95
mrrsmburu <mrrsmburu@gmail.com>
2023-10-07 17:05:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const drivers = ["Sally", "Bob", "Freddy", "Claudia"]
+
 const returnFirstTwoDrivers = function(drivers) {
     return drivers.slice(0, 2);
   };
@@ -8,45 +10,22 @@ const returnFirstTwoDrivers = function(drivers) {
   
   const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
   
-  const createFareMultiplier = function(integer) {
-    return function(fare) {
-      return fare * integer;
+  const createFareMultiplier = function(fareMultiplier) {
+    return function(value ) {
+      return fare * value;
     };
   };
   
   const fareDoubler = createFareMultiplier(2);
-  
   const fareTripler = createFareMultiplier(3);
   
   const selectDifferentDrivers = function(drivers, selectingFunction) {
     return selectingFunction(drivers);
   }
-  describe("returnFirstTwoDrivers", function() {
-    const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
-  
-    it("returns a new array containing the first two drivers from the passed-in array", function() {
-      expect(returnFirstTwoDrivers(drivers)).to.eql(["Sally", "Bob"]);
-    });
-  });
   
   
-  describe("returnLastTwoDrivers", function() {
-    const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
-  
-    it("returns an array of the last two drivers", function() {
-      expect(returnLastTwoDrivers(drivers)).to.eql(["Freddy", "Claudia"]);
-    });
-  });
   
   
-  describe("selectingDrivers", function() {
-    it("has the `returnFirstTwoDrivers` function to as its first element", function() {
-      expect(selectingDrivers[0]).to.equal(returnFirstTwoDrivers);
-    });
   
-    it("has the `returnLastTwoDrivers` function to as its last element", function() {
-      expect(selectingDrivers[1]).to.equal(returnLastTwoDrivers);
-    });
   
-    it("allows us to invoke either function from the array", function() {
-      const drivers = ["Sally", "Bob", "F
+  
